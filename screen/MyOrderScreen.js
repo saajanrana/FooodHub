2d77510167739156f5b8d02865e46b2a4ae0b389
saajanrana <saajanrana001@gmail.com>
2023-12-19 09:44:20 +0530

    Cart screen added

--- screen/MyOrderScreen.js
+++ screen/MyOrderScreen.js
@@ -1,11 +1,19 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React,{useState} from 'react';
+import {View, StyleSheet, Text,Switch,TouchableOpacity} from 'react-native';
+
 
 const MyOrderScreen = () => {
+
     return (
-        <View>
-            <Text>Hueeee hueeeee:</Text>
-        </View>
+        <View style={{backgroundColor:'#FFF',width:'100%',height:'100%'}} >
+            <View style={{borderWidth:2,borderColor:'#F2EAEA',width: 400,height:60,borderRadius:30,marginTop:20}}>
+
+                <TouchableOpacity style={{backgroundColor:'#FE724C',width: 150,height:40,borderRadius:20}}>
+                      <Text>Upcoming</Text>
+                </TouchableOpacity>
+                
+            </View>
+         </View>
     );
 }
 
